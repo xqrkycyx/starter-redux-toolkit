80516df82f67d04f6bfa8c6c7c03a98dd0645753
kchia <kchia87@gmail.com>
2022-06-15 10:46:34 -0400

    add complete code

--- src/features/notes/notes.slice.js
+++ src/features/notes/notes.slice.js
@@ -1,1 +1,43 @@
-// Add code here
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  notes: [
+    { id: 1, note: 'State is the current data in the app' },
+    { id: 2, note: 'View is the UI that displays the state' },
+    { id: 3, note: 'Actions alter the state of the app' },
+    { id: 4, note: 'Reducers should only make immutable updates' },
+    { id: 5, note: 'Reducers should be pure functions' },
+  ],
+};
+
+const options = {
+  name: 'notes',
+  initialState,
+  reducers: {
+    addNote: (state, action) => {
+      state.notes.push({ id: state.notes.length + 1, note: action.payload });
+    },
+    removeNote: (state, action) => {
+      state.notes.splice(
+        state.notes.findIndex((note) => note.id === action.payload),
+        1
+      );
+    },
+  },
+};
+
+const notesSlice = createSlice(options);
+
+// console.log(notesSlice);
+
+// console.log(notesSlice.actions);
+
+// console.log(
+//   notesSlice.actions.addNote(
+//     'A slice reducer updates a portion of the application state.'
+//   )
+// );
+
+const { addNote, removeNote } = notesSlice.actions;
+
+export default notesSlice.reducer;
